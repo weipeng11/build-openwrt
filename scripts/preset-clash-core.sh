912de1097b9773dc432f1504307100d8642b455f
haiibo <boyhaibo@gmail.com>
2024-11-15 21:24:37 +0800

    Initial commit

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -3,11 +3,13 @@
 mkdir -p files/etc/openclash/core
 
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+GEOIP_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
+GEOSITE_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
+COUNTRY_URL="https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb"
 
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $COUNTRY_URL > files/etc/openclash/Country.mmdb
 
-chmod +x files/etc/openclash/core/clash*
+chmod +x files/etc/openclash/core/clash_meta
